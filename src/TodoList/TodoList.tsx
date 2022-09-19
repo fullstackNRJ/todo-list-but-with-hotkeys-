900504a2179556a7d4a312196d1b890180d8590d
shreyasbg <shreyasbgangurde@gmail.com>
2022-09-20 01:33:45 +0530

    hotkeys not working

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,23 +1,45 @@
 import React from "react";
 import ToDoItem from "./TodoItem";
+import "./index.css";
 import { useTodoProvider } from "./useTodoList";
+import { HotKeys } from "react-hotkeys";
 
 type TodoListProps = {};
 
+const keyMap = {
+  ADD_A_TODO: "enter",
+  DELETE_A_TODO: "del",
+};
 const TodoList: React.FC<TodoListProps> = () => {
-  const { todoList, setTodoList } = useTodoProvider();
-
   const addATodo = () => {
     setTodoList([...todoList, ""]);
   };
+  const deleteLastTodo = () => {
+    const newList = [...todoList];
+    newList.pop();
+    setTodoList(newList);
+  };
+  const handlers = {
+    ADD_A_TODO: addATodo,
+    DELETE_A_TODO: deleteLastTodo,
+  };
+  const { todoList, setTodoList } = useTodoProvider();
 
   return (
-    <div>
-      <button onClick={addATodo}>ADD</button>
-      {todoList.map((item, index) => {
-        return <ToDoItem content={item} index={index} />;
-      })}
-    </div>
+    <HotKeys keyMap={keyMap}>
+      <div>
+        <div className="controlsbar">
+          <button onClick={addATodo}>ADD</button>
+        </div>
+        {todoList.map((item, index) => {
+          return (
+            <HotKeys handlers={handlers} key={`${index}+${item}`}>
+              <ToDoItem content={item} index={index} key={`${index}+${item}`} />
+            </HotKeys>
+          );
+        })}
+      </div>
+    </HotKeys>
   );
 };
 
